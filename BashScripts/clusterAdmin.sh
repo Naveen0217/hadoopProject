82bb4682d650c5b9dd7a2d540efd0dbe83403a02
hadoop <hadoop@newo.(none)>
2014-03-11 05:52:15 -0400

    expect automation script

--- BashScripts/clusterAdmin.sh
+++ BashScripts/clusterAdmin.sh
@@ -4,17 +4,17 @@
 # Author:      Michael Kepple
 # Called by:   ~/.bash_profile
 # Date:        29 Feb 2014
-# Note:        Requires SSHPass (yum install sshpass).
+# Note:        Needs updating.
 #
 source ~/.bashrc
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386
+export JAVA_HOME=/usr/java/jdk1.7.0_51
 export HADOOP_INSTALL=/home/hadoop/hadoop-1.2.1
 export PATH=$PATH:$HADOOP_INSTALL/bin:$JAVA_HOME/bin
 HADOOP_VERSION="hadoop-1.2.1"
 HBASE_VERSION="hbase-0.94.8"
 ZOOKEEPER_VERSION="zookeeper-3.4.5"
 HOSTNAME='echo $HOSTNAME | sed 's/\..*//''
-NODES=('nano' 'ammo' 'spino' 'techno' 'dryo' 'grypo' 'seismo' 'anon' 'rhino' 'aho' 'tito' 'tino' 'hippo' 'specko' 'muto' 'omego');
+NODES=('aho' 'tito' 'spino' 'nano' 'ammo' 'techno' 'dryo' 'grypo' 'anono' 'seismo' 'rhino' 'maino' 'newo' 'appo' 'drapo' 'mino' 'hippo' 'kepo');
 CLASS_ACER=('nano' 'ammo' 'spino' 'techno' 'dryo' 'grypo' 'seismo' 'anon');
 sshAgentInfo=$HOME/.ssh/agentInfo
 
@@ -39,20 +39,36 @@ node_delete()
     done
 }
 
+confDiff()
+{
+    echo "I'm a stub."
+}
+
 # Should be run as root/sudo'd
+# Note: master node must have installed sshpass
 admin_sync()
 {
     for node in ${NODES[@]}
     do
         sshpass -p $1 scp /etc/hosts $node:/etc/hosts
         sshpass -p $1 scp /home/hadoop/.ssh/authorized_keys $node:/home/hadoop/.ssh/authorized_keys
+	sshpass -p $1 ssh $node -t chown hadoop /home/hadoop/.ssh/authorized_keys
 	sshpass -p $1 scp /home/hadoop/.bash_profile $node:/home/hadoop/.bash_profile
 	sshpass -p $1 scp /home/hadoop/.bashrc $node:/home/hadoop/.bashrc
 	sshpass -p $1 scp /home/hadoop/clusterAdmin.sh $node:/home/hadoop/clusterAdmin.sh
     done
 }
 
-while getopts "h:d:z:x:s:" opt; do
+# Note: master node must have installed expect.
+init_passphrases()
+{
+    for node in ${NODES[@]}
+    do
+        /usr/bin/expect -f ./clusterExpect $node $1
+    done
+}	
+
+while getopts "h:d:z:x:s:i:" opt; do
     case $opt in
         x) node_delete $OPTARG
 	   ;;
@@ -64,6 +80,8 @@ while getopts "h:d:z:x:s:" opt; do
         z) echo "Zookeeper -> "
 	   #node_sync $ZOOKEEPER_VERSION
 	   ;;
+        i) init_passphrases $OPTARG
+           ;;
         s) admin_sync $OPTARG
 	   ;;
     esac
