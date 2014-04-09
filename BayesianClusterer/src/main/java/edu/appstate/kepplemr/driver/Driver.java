6f7169b98f1d94821265b777d42d512547883ef3
michael <kepplemr@gmail.com>
2014-04-09 02:05:52 -0400

    fixed bug in mahout's seqdirectory, using own version

--- BayesianClusterer/src/main/java/edu/appstate/kepplemr/driver/Driver.java
+++ BayesianClusterer/src/main/java/edu/appstate/kepplemr/driver/Driver.java
@@ -7,9 +7,6 @@ import org.apache.commons.cli2.builder.ArgumentBuilder;
 import org.apache.commons.cli2.builder.DefaultOptionBuilder;
 import org.apache.commons.cli2.builder.GroupBuilder;
 import org.apache.commons.cli2.commandline.Parser;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.mahout.text.SequenceFilesFromDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,19 +15,7 @@ public class Driver
 	private static final Logger log = LoggerFactory.getLogger(Driver.class);
 	
 	public static void main(String[] args)
-	{
-		Configuration.addDefaultResource("/etc/hadoop/conf/hdfs-site.xml");
-		Configuration.addDefaultResource("/etc/hadoop/conf/mapred-site.xml");
-		Configuration.addDefaultResource("/etc/hadoop/conf/yarn-site.xml");
-		Configuration.addDefaultResource("/etc/hadoop/conf/core-site.xml");
-		Configuration conf = new Configuration();
-		// When packaging with maven-assembly, included JARs can overwrite the proper conf settings; reset them. 
-		conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-		conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-		conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
-		conf.addResource(new Path("/etc/hadoop/conf/mapred-site.xml"));
-		conf.addResource(new Path("/etc/hadoop/conf/yarn-site.xml"));
-		
+	{		
 		DefaultOptionBuilder obuilder = new DefaultOptionBuilder();
 	    ArgumentBuilder abuilder = new ArgumentBuilder();
 	    GroupBuilder gbuilder = new GroupBuilder();
@@ -65,7 +50,8 @@ public class Driver
 	    
 	    try
 	    {
-	    	SequenceFilesFromDirectory.main(test);
+	    	// Use my modified seqdirectory class with conf bug fixed.
+	    	edu.appstate.kepplemr.seqdirectory.SequenceFilesFromDirectory.main(test);
 	    }
 	    catch (Exception ex)
 	    {
